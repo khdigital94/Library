f948bb53e02f6d0aef880a712c0d7b9ebc8b0ed5
Kevin Henrich <153145008+khdigital94@users.noreply.github.com>
2025-04-01 18:59:42 +0200

    cleaned up

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const bookContainer = document.querySelector("#bookContainer");
-    let myLibrary = [];
+	const bookContainer = document.querySelector("#bookContainer");
+	const addNewBookBtn = document.querySelector("#addNewBookBtn");
+	let myLibrary = [];
 
-    function Book(id, title, author, pages, price, url, imgurl) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.price = price;
-        this.url = url;
-        this.imgurl = imgurl;
-    }
+	function Book(id, title, author, pages, price, url, imgurl) {
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.price = price;
+		this.url = url;
+		this.imgurl = imgurl;
+	}
 
-    function addBookToLibrary(title, author, pages, price, url, imgurl) {
-        let bookId = crypto.randomUUID();
-        myLibrary.push(new Book(bookId, title, author, pages, price, url, imgurl))
-    }
+	function addBookToLibrary(title, author, pages, price, url, imgurl) {
+		let bookId = crypto.randomUUID();
+		myLibrary.push(new Book(bookId, title, author, pages, price, url, imgurl));
+	}
 
-    addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 842, 49.99, "https://thegreatestbooks.org/books/38", "https://images.thegreatestbooks.org/ymseqoyhkyhpho50rs2jdjmi2gnn");
-    addBookToLibrary("Ulysses", "James Joyce", 1240, 69.99, "https://thegreatestbooks.org/books/122", "https://images.thegreatestbooks.org/sbd37b2dsyuw15cv63l87biw63kv");
-    addBookToLibrary("In Search of Lost Time", "Marcel Proust", 381, 29.99, "https://thegreatestbooks.org/books/225", "https://images.thegreatestbooks.org/myvbhitdua7h1etye2hvfjej2p4j");
-    addBookToLibrary("One Hundred Years of Solitude", "Gabriel García Márquez", 837, 49.99, "https://thegreatestbooks.org/books/266", "https://images.thegreatestbooks.org/fzce7ac1jcmx6fi8ppnea65ct3u9");
+	addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 842, 49.99, "https://thegreatestbooks.org/books/38", "https://images.thegreatestbooks.org/ymseqoyhkyhpho50rs2jdjmi2gnn");
+	addBookToLibrary("Ulysses", "James Joyce", 1240, 69.99, "https://thegreatestbooks.org/books/122", "https://images.thegreatestbooks.org/sbd37b2dsyuw15cv63l87biw63kv");
+	addBookToLibrary("In Search of Lost Time", "Marcel Proust", 381, 29.99, "https://thegreatestbooks.org/books/225", "https://images.thegreatestbooks.org/myvbhitdua7h1etye2hvfjej2p4j");
+	addBookToLibrary("One Hundred Years of Solitude", "Gabriel García Márquez", 837, 49.99, "https://thegreatestbooks.org/books/266", "https://images.thegreatestbooks.org/fzce7ac1jcmx6fi8ppnea65ct3u9");
 
-    const displayBooks = () => {
-        bookContainer.innerHTML = "";
-        myLibrary.forEach((book) => {
-            let bookCard = `
+	const displayBooks = () => {
+		bookContainer.innerHTML = "";
+		myLibrary.forEach((book) => {
+			let bookCard = `
                     <article class="bookCard" data-id="${book.id}">
                         <a href="${book.url}">
                             <div class="imgContainer">
@@ -41,25 +42,29 @@ document.addEventListener("DOMContentLoaded", () => {
                         <button class="removeBookButton" type="button">Remove Book</button>
                     </article>
             `;
-            bookContainer.innerHTML += bookCard;
-        })
-    }
+			bookContainer.innerHTML += bookCard;
+		});
+	};
 
-    // Displays books on page load
-    displayBooks();
+	// Displays books on page load
+	displayBooks();
 
-    const removeBook = (bookId) => {
-        const bookToRemove = myLibrary.find((book) => book.id == bookId);
-        myLibrary = myLibrary.filter(book => book !== bookToRemove)
-    }
+	addNewBookBtn.addEventListener("click", () => {
+		console.log("Works");
+	});
 
-    bookContainer.addEventListener("click", (e) => {
-        if (e.target.classList.contains("removeBookButton")) {
-            const bookCard = e.target.closest(".bookCard");
-            const bookId = bookCard.dataset.id;
+	const removeBook = (bookId) => {
+		const bookToRemove = myLibrary.find((book) => book.id == bookId);
+		myLibrary = myLibrary.filter((book) => book !== bookToRemove);
+	};
 
-            removeBook(bookId);
-            displayBooks();
-        }
-    })
-})
+	bookContainer.addEventListener("click", (e) => {
+		if (e.target.classList.contains("removeBookButton")) {
+			const bookCard = e.target.closest(".bookCard");
+			const bookId = bookCard.dataset.id;
+
+			removeBook(bookId);
+			displayBooks();
+		}
+	});
+});
